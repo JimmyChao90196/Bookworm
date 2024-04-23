9747d154f14672398c60b1a82fe4bc6cbfdd876c
Jimmy <myname90196@gmail.com>
2024-04-23 19:42:58 +0800

    Initial Commit

---
+++ SwiftUI-Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Bookworm
+//
+//  Created by JimmyChao on 2024/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Bookworm/SwiftUI_BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BookwormApp.swift
+//  SwiftUI-Bookworm
+//
+//  Created by JimmyChao on 2024/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
